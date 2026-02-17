3aae5b969252efecfbd62aab65928429ca31b456
jmorgan28-01 <jtmorgan301@gmail.com>
2026-02-17 16:00:37 -0600

    changed ocean color

--- src/scene/setupScene.js
+++ src/scene/setupScene.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 
 export function createScene() {
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x001e0f);
-  scene.fog = new THREE.Fog(0x001e0f, 10, 50);
+  scene.background = new THREE.Color(0x0a1f2f);
+  scene.fog = new THREE.FogExp2(0x0a1f2f, 0.02);
   return scene;
 }
